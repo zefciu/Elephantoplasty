bc624a8c5d3117878cd1ef1b08bb7b34fc69e7a8
Szymon Pyżalski <zefciu@gmail.com>
2011-07-18 07:37:41 +0200

    Changed the hydration logic

---
+++ src/eplasty/field/one_column.py
@@ -0,0 +1,29 @@
+from base import Field
+
+class OneColumn(Field):
+    '''
+    Base class for all field that just proxy one column
+    '''
+
+    def __init__(self, name=None, *args, **kwargs):
+        self.name = name
+        self.column = self.ColumnType(name=name, *args, **kwargs)
+        self.columns = [self.column]
+        super(OneColumn, self).__init__(name, *args, **kwargs)
+
+    def _get_value(self, inst, cls):
+        return self.column._get_value(self, inst, cls)
+
+    def get_c_vals(self, dict_):
+        """Gets a mapping of column_name -> column_value for this field"""
+        if self.name in dict_:
+            return {self.column.name: dict_[self.name]}
+        else:
+            return {}
+
+    def bind_class(self, cls, name):
+        self.column.name = name
+        return super(OneColumn, self).bind_class(cls, name)
+
+    def hydrate(self, inst, col_vals, dict_):
+        dict_[self.name] = col_vals[self.column.name]
