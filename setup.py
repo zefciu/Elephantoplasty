bab2a2745480a02f522aba77ee37b80c1701a13c
Szymon Pyżalski <zefciu@gmail.com>
2011-07-27 17:31:52 +0200

    Corrected package list in setup.py

--- setup.py
+++ setup.py
@@ -16,5 +16,7 @@ setup(
       install_requires = ['psycopg2>=2.4.1'],
       setup_requires = ['nose>=1.0', 'nose-cov>=1.0'],
       package_dir = {'': 'src'},
-      packages = ['eplasty'],
+      packages = [
+          'eplasty', 'eplasty.field', 'eplasty.object', 'eplasty.relation'
+      ],
 )
