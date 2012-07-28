e15002c02ed8787a1a6ab906ee39a80859d49edf
Szymon Pyżalski <szymon.pyzalski@stxnext.pl>
2012-07-28 19:24:23 +0200

    Created adapter field module (untested yet)

--- src/eplasty/field/__init__.py
+++ src/eplasty/field/__init__.py
@@ -2,3 +2,4 @@ from eplasty.field.base import Field
 from eplasty.field.simple import *
 from eplasty.field.helper import *
 from eplasty.field.file import FileField
+from eplasty.field.adapter import *
---
+++ src/eplasty/field/adapter.py
@@ -0,0 +1,53 @@
+"""Contains AdapterField base class and some common subclasses of it."""
+
+import abc
+try:
+    from lxml import html
+except ImportError:
+    html = None
+from eplasty.field.one_column import OneColumn
+from eplasty import column as c
+
+class AdapterField(OneColumn):
+    """AdapterField is a single-column field that knows how to transform a
+    selected python type to another. It doesn't create a custom type in the
+    backend (unlike TODO: CustomField)."""
+
+    @abc.abstractproperty
+    def python_types(self):
+        pass
+
+    def _is_compatible(self, value):
+        return isinstance(value, self.python_types)
+
+    def get_c_vals(self, dict_):
+        if self.name in dict_:
+            return {self.column.name: self.from_python(dict_[self.name])}
+        else:
+            return {}
+
+    def hydrate(self, inst, col_vals, session):
+        dict_[self.name] = self.to_python(col_vals[self.column.name])
+
+class Missing:
+    """This class raises error when instantiated."""
+    def __init__(self):
+        raise self.err
+
+if html:
+    class Html(AdapterField):
+        """Html is represented by an etree on python side and by text on
+        postgres side."""
+
+        python_types = html.HtmlElement
+        ColumnType = c.Text
+
+        def to_python(self, value):
+            return html.fromstring(value)
+
+        def from_python(self, value):
+            return html.tostring(value, encoding='unicode')
+else:
+    class Html(Missing):
+        """Html is missing. You need to install lxml."""
+        err = ImportError('You need to install lxml package to use HTMLField')
